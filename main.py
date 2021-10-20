caf39a346f1f530b6e9e3e5e5ca5770f3d48c4da
Euan <Euanmeston@gmail.com>
2021-10-20 22:06:44 +0100

    WIP

--- main.py
+++ main.py
@@ -54,7 +54,13 @@ class DatabaseScraper:
         table_rows.insert(0, table_headers)
         return table_rows
 
-    def strip_index_table(self, page_text: str):
+    def tier_check(self, index_table):
+        for meets in index_table[1::]:
+            if meets[1] not in ['Tier 1', 'Tier 2', 'Tier 3', 'International']:
+                meets.insert(1, "None")
+        return index_table
+
+    def strip_index_table(self):
         index_page = self.BROWSER_SESSION.get(self.EVENT_INDEX)
         result_table_id = "ranking-matches-resources"
         soup = BeautifulSoup(index_page.text, "html.parser")
@@ -63,19 +69,31 @@ class DatabaseScraper:
         table_rows = get_table_rows(result_table, True)
         table_rows.insert(0, table_headers)
         # Todo - make this shit modular...and the strip_results_table thing
-        for rows in table_rows:
-            print(rows)
-        return table_rows
+        parsed_table = self.tier_check(table_rows)
+        return parsed_table
 
     def get_event_result(self, event_id: int):
         event_page = self.BROWSER_SESSION.get(f"{self.EVENT_INDEX}&resource={event_id}")
-        return event_page.text
+        return self.strip_result_table(event_page.text)
 
     def main(self):
         with open("result_page.txt", 'r') as saved_file:
             result_page = saved_file.read()
         results = self.strip_result_table(result_page)
         self.data_analysis(results)
+
+    def fetch_meets_by_year(self, year: int):
+        """
+        Probably going to make this a filtering function or something and
+        dump the parsed index to a CSV type database
+        """
+        meet_options = self.strip_index_table()
+        with open(f"meets_{year}.csv", "w") as csv_file:
+            csvwrite = csv.writer(csv_file)
+            csvwrite.writerow(meet_options[0])
+            for meets in meet_options:
+                if str(year) in meets[2]:
+                    csvwrite.writerow(meets)
 
     def data_analysis(self, results_csv):
         """ OK so this bit is working """
@@ -85,5 +103,6 @@ class DatabaseScraper:
 
 if __name__ == '__main__':
     scraper = DatabaseScraper()
-    scraper.strip_index_table(None)
+    # scraper.strip_index_table()
     # scraper.main()
+    scraper.fetch_meets_by_year(2021)
